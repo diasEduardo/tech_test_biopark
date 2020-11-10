dded0340a5ae098a6cda3feccbd9dede73561579
diasEduardo <eduardo_dududex@hotmail.com>
2020-11-10 18:04:45 -0300

    add fix on sql timestamp to json conversion

---
+++ source/api/api.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import sys
+sys.path.append('../database')
+import db
+
+app = Flask(__name__)
+
+@app.route('/api/agendamentos',methods=['GET'])
+def list_all_schedule():
+	query = "SELECT s.id, st.status, t.type, r.name, r.email, r.phone, s.date_time, s.message "\
+			"FROM send_schedule AS s "\
+			"JOIN send_status AS st ON s.status_id = st.id "\
+			"JOIN send_type AS t ON s.type_id = t.id "\
+			"JOIN receiver AS r ON s.receiver_id = r.id "
+	print(query);
+	response = db.exec(query);
+	print(response);
+	return response
+
+
+
+
+app.run()
--- source/database/db.py
+++ source/database/db.py
@@ -4,8 +4,13 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from config_parser import config
 import json
+from datetime import datetime
 
 params = config("C:\\Users\\Eduardo\\Documents\\git_projects\\tech_test_biopark\\source\\database\\db.ini","postgresql")
+
+def dt_parser(dt):
+    if isinstance(dt, datetime):
+        return dt.isoformat()
 
 def exec(query):
 	conn = None
@@ -17,7 +22,7 @@ def exec(query):
 		cur = conn.cursor(cursor_factory=RealDictCursor)
 		cur.execute(query)
 		
-		query_response = json.dumps(cur.fetchall(), indent=2)
+		query_response = json.dumps(cur.fetchall(), indent=2,default=dt_parser)
 		cur.close()
 	except (Exception, psycopg2.DatabaseError) as error:
 		print(error)
@@ -28,5 +33,10 @@ def exec(query):
 
 
 if __name__ == '__main__':
-    response = exec("SELECT * FROM send_type LIMIT 5")
-    print(response)
+	query = "SELECT s.id, st.status, t.type, r.name, r.email, r.phone, s.date_time, s.message "\
+		"FROM send_schedule AS s "\
+		"JOIN send_status AS st ON s.status_id = st.id "\
+		"JOIN send_type AS t ON s.type_id = t.id "\
+		"JOIN receiver AS r ON s.receiver_id = r.id "
+	response = exec(query)
+	print(response)
