4332f946c8739f764c3728a104a984e9c75b405c
diasEduardo <eduardo_dududex@hotmail.com>
2020-11-10 20:58:00 -0300

    added insert

--- source/api/api.py
+++ source/api/api.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 import sys
 sys.path.append('../database')
 import db
@@ -25,6 +26,37 @@ def select_schedule(select_id):
 			"WHERE s.id = " + str(select_id)
 	response = db.select(query);
 	return response
+
+@app.route('/api/agendamentos',methods=['POST'])
+def insert_schedule():
+	output = "erro ao inserir"
+	code = 400
+	erro_flag = False
+
+	status  = 1
+	type1 = int(request.args.get('type_id'))
+	receiver = int(request.args.get('receiver_id'))
+	date = request.args.get('date_time')
+	message = request.args.get('message')
+	if(type1 is None):
+		erro_flag = True 
+	elif(receiver is None):
+		erro_flag = True
+	elif(date is None):
+		erro_flag = True
+	elif(message is None):
+		message = ""
+	
+
+	if( not erro_flag):
+		query = "INSERT INTO send_schedule(status_id,type_id,receiver_id,date_time,message) "\
+			"values(%d, %d, %d, '%s', '%s')"%(status, type1, receiver, date, message)
+		response = db.exec(query);
+		if(response):
+			output = "inserido com sucesso"
+			code = 200
+
+	return output, code
 
 @app.route('/api/agendamentos/<delete_id>',methods=['DELETE'])
 def delete_schedule(delete_id):
