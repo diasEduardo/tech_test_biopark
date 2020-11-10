d999415aec8bbe0f83e52eb74717d1586e241599
diasEduardo <eduardo_dududex@hotmail.com>
2020-11-10 19:18:11 -0300

    added sql delete function

--- source/database/db.py
+++ source/database/db.py
@@ -31,12 +31,28 @@ def exec(query):
 			conn.close()
 	return query_response
 
+def delete(query):
+	conn = None
+	query_response = None
+	try:
+		
+		conn = psycopg2.connect(**params)
+		
+		cur = conn.cursor(cursor_factory=RealDictCursor)
+		cur.execute(query)
+		query_response = cur.rowcount;
+		conn.commit();
+		cur.close()
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			conn.close()
+	return query_response
+
 
 if __name__ == '__main__':
-	query = "SELECT s.id, st.status, t.type, r.name, r.email, r.phone, s.date_time, s.message "\
-		"FROM send_schedule AS s "\
-		"JOIN send_status AS st ON s.status_id = st.id "\
-		"JOIN send_type AS t ON s.type_id = t.id "\
-		"JOIN receiver AS r ON s.receiver_id = r.id "
-	response = exec(query)
+	query = "DELETE FROM send_schedule "\
+			"WHERE id = 7" 
+	response = delete(query)
 	print(response)
