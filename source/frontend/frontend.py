6b57a37730c92541ff1ddbc38f17d2ffdc034dc9
diasEduardo <eduardo_dududex@hotmail.com>
2020-11-11 15:50:29 -0300

    added frontend insert form

--- source/frontend/frontend.py
+++ source/frontend/frontend.py
@@ -1,9 +1,23 @@
 from flask import Blueprint
-
+from flask import render_template
+from flask import request
+import json
+import sys
+sys.path.append('../database')
+sys.path.append('..')
+import db
+from index import app
 
 frontend_app = Blueprint('frontend_app', __name__)
 
 
-@frontend_app.route('/', methods=['GET'])
+@frontend_app.route('/', methods=['GET','POST'])
 def home_page():
-	return "HOME PAGE HERE"
+	if request.method == 'POST':
+		a=1
+	types = json.loads(db.select("SELECT id, type from send_type"))
+	receivers = json.loads(db.select("SELECT id, name from receiver"))
+	#send_schedule = json.loads(app.get('/api/agendamentos'))
+	print(send_schedule)
+	return render_template('home_page.html', types=types, receivers=receivers,send_schedule=send_schedule)
+
