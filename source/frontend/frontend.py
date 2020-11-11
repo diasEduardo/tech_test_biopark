49b877cb37a95c384401da4ebcdfcab10bcc3618
diasEduardo <eduardo_dududex@hotmail.com>
2020-11-11 18:24:32 -0300

    added list of schedules on frontend

--- source/frontend/frontend.py
+++ source/frontend/frontend.py
@@ -1,23 +1,25 @@
 from flask import Blueprint
 from flask import render_template
 from flask import request
+import requests
 import json
 import sys
 sys.path.append('../database')
 sys.path.append('..')
 import db
-from index import app
 
 frontend_app = Blueprint('frontend_app', __name__)
 
 
 @frontend_app.route('/', methods=['GET','POST'])
 def home_page():
+	insert_msg = ""
+	delete_msg = ""
 	if request.method == 'POST':
-		a=1
+		insert_msg = "entrou no post"
+		#todo
 	types = json.loads(db.select("SELECT id, type from send_type"))
 	receivers = json.loads(db.select("SELECT id, name from receiver"))
-	#send_schedule = json.loads(app.get('/api/agendamentos'))
-	print(send_schedule)
-	return render_template('home_page.html', types=types, receivers=receivers,send_schedule=send_schedule)
+	send_schedule = json.loads(requests.get(request.base_url  + '/api/agendamentos').text)
+	return render_template('home_page.html', types=types, receivers=receivers,send_schedule=send_schedule,insert_msg=insert_msg, delete_msg=delete_msg)
 
