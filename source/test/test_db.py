2579714530a82912cc63275cfd57287a58caddba
diasEduardo <eduardo_dududex@hotmail.com>
2020-11-10 21:07:00 -0300

    added insertion test

--- source/test/test_db.py
+++ source/test/test_db.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../database')
 import db
+import json
 
 def test_db():
 	#select
@@ -12,9 +13,13 @@ def test_db():
 	assert db.select("SELECT id, date_time FROM send_schedule LIMIT 1") != None
 
 	#insert
-	id = 15
+	query = "INSERT INTO send_schedule(status_id,type_id,receiver_id,date_time,message) "\
+			"values(%d, %d, %d, '%s', '%s')"%( 4, 4, 1, '2020-12-21 11:00:54', "auto teste")
+	insert = db.exec(query)
+	assert insert > 0
+	response = json.loads(db.select("SELECT id FROM send_schedule ORDER BY id DESC LIMIT 1 "))
 	#delete
-	delete = db.exec("DELETE FROM send_schedule where id = "+str(id))
+	delete = db.exec("DELETE FROM send_schedule where id = "+str(response[0]['id']))
 	assert delete == 0 or delete == 1
 
 test_db();
